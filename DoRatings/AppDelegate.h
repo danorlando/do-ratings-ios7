4304d661e8f78abfcc7cd873595181e208fe2423
danorlando <danorlando1@me.com>
2013-11-07 19:49:38 -0700

    Initial Commit

---
+++ DoRatings/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DoRatings
+//
+//  Created by Dan Orlando on 11/7/13.
+//  Copyright (c) 2013 Dan Orlando. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DoRatings/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DoRatings
+//
+//  Created by Dan Orlando on 11/7/13.
+//  Copyright (c) 2013 Dan Orlando. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
